bd01ab6507e5394b2cc0e6b7cdd80284dfd33516
wolframcrem <wolfram@localhost>
2021-04-15 21:19:24 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button @click="init">hello</button>
+  <h2>{{piholeStatsElasticsearch}}</h2>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import mqtt from 'mqtt';
 
 export default {
   name: 'App',
+
+  props: ['stats'],
+  data() {
+    return {
+      piholeStats: this.stats
+    }
+  },
+
   components: {
     HelloWorld
+  },
+  methods: {
+    init() {
+      this.piholeStats = [{}];
+      const client = mqtt.connect('ws://broker.hivemq.com:8000/mqtt');
+
+      client.on('connect', () => {
+        client.subscribe('/testgroupname/stats/pihole');
+      });
+
+      client.on('message', (topic, message) => {
+        const data = JSON.parse(message.toString());
+        console.log(data);
+        this.piholeStats.push(data)
+      });
+    }
   }
 }
 </script>
